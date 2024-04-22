c4cda4e8b3314c232855ad0d711e5dcdd21cdbf1
susanpamela2520 <3030172520108@ingenieria.usac.edu.gt>
2024-04-21 19:50:44 -0600

    commit

--- Frontend/CompiScipt+/src/App.jsx
+++ Frontend/CompiScipt+/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Menu, MenuButton, MenuList, MenuItem, MenuDivider, Card } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+<>
+<Card>
+
+<Card
+direction="row"
+px={4}
+gap={2}
+bg={"gray.600"}
+>
+
+<Menu>
+          <MenuButton
+            px={4}
+            py={2}
+            transition='all 0.2s'
+            borderRadius='md'
+            borderWidth='1px'
+            bg={"gray.200"}
+            _hover={{ bg: 'green.100' }}
+            _expanded={{ bg: 'green.200' }}
+            _focus={{ boxShadow: 'outline' }}
+          >
+            Archivo <ChevronDownIcon />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Crear Archivos</MenuItem>
+            <MenuDivider />
+            <MenuItem>Abrir Archivos</MenuItem>
+            <MenuDivider />
+            <MenuItem>Guardar Archivo</MenuItem>
+            <MenuDivider />
+          </MenuList>
+          </Menu>
+
+          <Menu>
+          <MenuButton
+            px={4}
+            py={2}
+            transition='all 0.2s'
+            borderRadius='md'
+            borderWidth='1px'
+            bg={"gray.200"}
+            _hover={{ bg: 'green.100' }}
+            _expanded={{ bg: 'green.200' }}
+            _focus={{ boxShadow: 'outline' }}
+          >
+            Herramienta <ChevronDownIcon />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Ejecutar</MenuItem>
+            <MenuDivider />
+          
+          </MenuList>
+
+
+        </Menu>
+
+
+
+        <Menu>
+          <MenuButton
+            px={4}
+            py={2}
+            transition='all 0.2s'
+            borderRadius='md'
+            borderWidth='1px'
+            bg={"gray.200"}
+            _hover={{ bg: 'green.100' }}
+            _expanded={{ bg: 'green.200' }}
+            _focus={{ boxShadow: 'outline' }}
+          >
+            Reporte <ChevronDownIcon />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Reporte de Errores </MenuItem>
+            <MenuDivider />
+            <MenuItem>Generar Arbol AST </MenuItem>
+            <MenuDivider />
+          
+          </MenuList>
+
+
+        </Menu>
+
+
+
+
+
+</Card>
+
+</Card>
+</>
+  );
 }
 
 export default App
